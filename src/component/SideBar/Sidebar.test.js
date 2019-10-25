374e9dfa9a6ecb3511ee3d5879c82138712b43e8
MouhmaedEssam <moessam@devsquads.com>
2019-10-25 15:17:14 +0200

    Initial commit from Create React App

---
+++ src/component/SideBar/Sidebar.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {shallow} from "enzyme";
+import Sidebar from "./Sidebar";
+
+describe('Sidebar', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Sidebar/>);
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+});
