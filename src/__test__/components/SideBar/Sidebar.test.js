05e3cd59124b32a8a1efb790101b6ac53c62e69c
MouhmaedEssam <moessam@devsquads.com>
2019-10-30 15:33:48 +0200

    Add cypress for e2e2 testing

--- src/__test__/components/SideBar/Sidebar.test.js
+++ src/__test__/components/SideBar/Sidebar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Sidebar from './Sidebar';
+import Sidebar from '../../../components/SideBar/Sidebar';
 
 describe('Sidebar', () => {
   it('renders without crashing', () => {
