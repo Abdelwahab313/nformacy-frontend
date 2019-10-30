b1171f3365b6ed7da44de5dac9c68160379b220c
MouhmaedEssam <moessam@devsquads.com>
2019-10-30 13:53:22 +0200

    add linting tool in pree commit hook

--- src/component/SideBar/Sidebar.test.js
+++ src/component/SideBar/Sidebar.test.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import {shallow} from "enzyme";
-import Sidebar from "./Sidebar";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Sidebar from './Sidebar';
 
 describe('Sidebar', () => {
   it('renders without crashing', () => {
-    const wrapper = shallow(<Sidebar/>);
+    const wrapper = shallow(<Sidebar />);
     expect(wrapper.find('div').length).toEqual(1);
   });
 });
