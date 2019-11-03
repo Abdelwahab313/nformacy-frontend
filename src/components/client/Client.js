700474a846277454a9d23527f1512304eaa0e3eb
Ahmed Mohsen <xa.mohsen@gmail.com>
2019-11-03 15:32:58 +0200

    add client details card

--- src/components/client/Client.js
+++ src/components/client/Client.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,22 +15,42 @@ const Client = (props) => {
   const classes = useStyles();
 
   return (
-    <div dir='rtl' className={classes.root}>
+    <div className={classes.root} dir='rtl'>
       <Paper className={classes.root}>
-        <Typography variant='h5' component='h3'>
-          تفاصيل العميل
-        </Typography>
+        <p>تفاصيل العميل</p>
         <Table>
-          <TableHead>
+          <TableRow>
+            <TableCell>أسم المكان</TableCell>
+            <TableCell>{props.client.name}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>أسم المدير</TableCell>
+            <TableCell>{props.client.ownerName}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>العنوان</TableCell>
             <TableRow>
-              <TableCell>أسم المكان</TableCell>
-              <TableCell>{props.client.name}</TableCell>
+              <TableCell>الموقع</TableCell>
+              <TableCell>
+                {props.client.location.long}, {props.client.location.lat}
+              </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>أسم المدير</TableCell>
-              <TableCell>{props.client.ownerName}</TableCell>
+              <TableCell>العنوان</TableCell>
+              <TableCell>{props.client.address}</TableCell>
             </TableRow>
-          </TableHead>
+          </TableRow>
+          <TableRow>
+            <TableCell>التليفون</TableCell>
+            <TableCell>
+              {props.client.phones.map((phone) => (
+                <>
+                  {' '}
+                  {phone} <br />
+                </>
+              ))}
+            </TableCell>
+          </TableRow>
         </Table>
       </Paper>
     </div>
