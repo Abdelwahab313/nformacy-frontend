9acb3db641514504472a937ec486bbb997fbf632
amlmoustafa <amal@devsquads.com>
2019-11-19 20:12:58 +0200

    added startDate and endDate view

--- src/apis/salesApi.js
+++ src/apis/salesApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../settings';
 
-const getSalesWithDate = (start_date, end_date, token) => {
+export const getSalesWithDate = (start_date, end_date, token) => {
   axios({
     method: 'get',
     url: `${API_BASE_URL}/sales/?start_date=${start_date}&end_date=${end_date}`,
--- src/components/sales/SalesList.js
+++ src/components/sales/SalesList.js
@@ -5,8 +5,10 @@ import Paper from '@material-ui/core/Paper';
 import MaterialTable from 'material-table';
 import { table_localization } from '../../settings';
 import { sales } from '../../data';
-import moment from 'moment';
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Button } from '@material-ui/core';
+import { getSalesWithDate } from '../../apis/salesApi';
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(3),
@@ -33,11 +35,28 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 });
 const SalesList = (props) => {
   const classes = useStyles();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   return (
     <div className={classes.root} dir='rtl'>
       <Grid container className={classes.details}>
         <Grid item lg={7} md={8} xs={12}>
+          <Paper>
+            <label>تاريخ البدء</label>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
+            <label>تاريخ الانتهاء</label>
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+            />
+            <Button onClick={getSalesWithDate} color='primary'>
+              عرض
+            </Button>
+          </Paper>
           <Paper>
             <MaterialTable
               id={'salesList'}
