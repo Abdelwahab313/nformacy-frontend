ece3ec9deea8b2383ef9154cbe68202c8c2e0e68
Mohammed <m.wahdan@devsquads.com>
2020-07-09 14:39:31 +0200

    modify success page style

--- src/styles/successStyle.js
+++ src/styles/successStyle.js
@@ -4,7 +4,41 @@ export const useStyles = makeStyles((theme) => ({
   successRootContainer: {
     height: '100vh',
   },
-  successMessageContainer: { height: '100vh', paddingTop: 50 },
-  successText: { textAlign: 'center', marginTop: 50 },
-  subtext: { alignSelf: 'flex-end' },
+  successMessageContainer: {
+    height: '100vh',
+    paddingTop: 50,
+  },
+  successText: {
+    textAlign: 'center',
+    marginTop: 50,
+    fontSize: '1.85vw',
+    fontWeight: '600',
+    display: 'flex',
+    flexDirection: 'column',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '3vw',
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '5vw',
+    },
+    [theme.breakpoints.up('xl')]: {
+      fontSize: '1.00vw',
+    },
+  },
+  subtext: {
+    alignSelf: 'flex-end',
+    fontSize: '1.190vw',
+    fontWeight: '600',
+    justifySelf: 'flex-end',
+    textAlign: 'left',
+    [theme.breakpoints.up('xl')]: {
+      fontSize: '0.9vw',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2.604vw',
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '4vw',
+    },
+  },
 }));
