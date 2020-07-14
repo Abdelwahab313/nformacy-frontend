fc451803203e8b150beae0f2d7970f6432f8fdc3
Abdelwahab <abdelwahab@devsquads.com>
2020-07-14 12:09:14 +0200

    add link to profile from summary card

--- src/pages/Home/ProfileSummaryCard.js
+++ src/pages/Home/ProfileSummaryCard.js
@@ -5,25 +5,28 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import useStyles from './styles/HomePageStyles';
+import { Link } from 'react-router-dom';
 
 const ProfileSummaryCard = () => {
   const classes = useStyles();
 
   return (
     <Card className={classes.card}>
-      <CardActionArea>
+      <CardActionArea
+        component={Link}
+        to={'/user/edit'}>
         <CardMedia
           className={classes.media}
           title="Contemplative Reptile"
         />
         <CardContent>
-          <Typography  align={'center'} gutterBottom variant="h5" component="h2">
+          <Typography align={'center'} gutterBottom variant="h5" component="h2">
             Profile
           </Typography>
           <Typography align={'center'} variant="body2" color="textSecondary" component="p">
             Expert in the (Fields/subfields)
           </Typography>
-          <Typography  align={'center'} variant="body2" color="textSecondary" component="p">
+          <Typography align={'center'} variant="body2" color="textSecondary" component="p">
             In these industries
           </Typography>
         </CardContent>
