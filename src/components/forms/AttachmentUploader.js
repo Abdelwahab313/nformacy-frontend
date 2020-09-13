f6227db34334959b178968fe8964c8c2ce1d0310
mohamedalzaki <mohamedalzaki@outlook.com>
2020-09-13 10:52:03 +0200

    Check attachment length before uploading

--- src/components/forms/AttachmentUploader.js
+++ src/components/forms/AttachmentUploader.js
@@ -8,69 +8,66 @@ import {
   attachButtonStyle,
   attachContainerStyle,
 } from '../../styles/questionRoasterStyles';
-import {
-  removeAttachment,
-  uploadAttachment
-} from '../../apis/questionsAPI';
+import { removeAttachment, uploadAttachment } from '../../apis/questionsAPI';
 import t from '../../locales/en/questionRoaster.json';
 import FileUpload from '../inputs/FileUpload';
 
-
 const styles = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flexWrap: "wrap",
-  width: "100%"
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+  width: '100%',
 };
-
 
 const AttachmentUploader = ({
   containerClassName,
   attachments,
   attachmentsGroupsId,
-  setAttachmentsGroupsId
+  setAttachmentsGroupsId,
 }) => {
   const [attachmentFiles, setAttachmentFiles] = useState(attachments);
 
-  console.log('------ attachments', attachments)
-  console.log('------ attachments', attachmentFiles)
+  console.log('------ attachments', attachments);
+  console.log('------ attachments', attachmentFiles);
 
   const onUploadAttachment = (attachmentFile) => {
     const fileBlob = new Blob(attachmentFile);
     const formData = new FormData();
+    if (attachmentFile.length === 0) return;
     formData.append('attachment[]', fileBlob, attachmentFile[0].name);
     formData.append('attachments_groups_id', attachmentsGroupsId);
 
     uploadAttachment(formData).then((response) => {
       const returnedAttachments = response.data.attachments;
       if (returnedAttachments.length >= 1) {
-        setAttachmentsGroupsId(response.data.attachmentsGroupsId)
+        setAttachmentsGroupsId(response.data.attachmentsGroupsId);
         setAttachmentFiles(returnedAttachments);
-      } else {
-        console.log('something error with attachments');
       }
     });
   };
 
   const onDeleteAttachment = (attachmentId) => {
-    removeAttachment(attachmentId)
-      .then((response) => {
-        console.log('file deleted');
-        const attachmentAfterDelete = attachmentFiles.filter((attachment) => (attachment.signedId !== attachmentId));
-        setAttachmentFiles(attachmentAfterDelete);
-       });
+    removeAttachment(attachmentId).then((response) => {
+      const attachmentAfterDelete = attachmentFiles.filter(
+        (attachment) => attachment.signedId !== attachmentId,
+      );
+      setAttachmentFiles(attachmentAfterDelete);
+    });
   };
-
 
   const renderPreviewFiles = () => {
     return attachmentFiles.map((file, index) => {
-      console.log('picture', file)
+      console.log('picture', file);
       return (
         <Grid key={index} item xs={12}>
           <Chip
             key={index}
-            icon={<IconButton onClick={() => window.open(file.url)}><DownloadIcon /></IconButton>}
+            icon={
+              <IconButton onClick={() => window.open(file.url)}>
+                <DownloadIcon />
+              </IconButton>
+            }
             id={`attachment-${index}`}
             label={file.filename}
             onDelete={() => onDeleteAttachment(file.signedId)}
@@ -78,11 +75,11 @@ const AttachmentUploader = ({
         </Grid>
       );
     });
-  }
+  };
 
   return (
     <div className={containerClassName}>
-      <Grid container direction="column" alignItems={'flex-start'}>
+      <Grid container direction='column' alignItems={'flex-start'}>
         <FileUpload
           label={''}
           fileContainerStyle={attachContainerStyle()}
@@ -90,12 +87,14 @@ const AttachmentUploader = ({
           withPreview={false}
           onChange={onUploadAttachment}
           withPreview
-          accept=".doc,.docx,.xml,image/*,video/*,.pdf,.csv"
+          accept='.doc,.docx,.xml,image/*,video/*,.pdf,.csv'
           buttonStyles={attachButtonStyle()}
           buttonText={t['attach']}
         />
-        <FlipMove enterAnimation="fade" leaveAnimation="fade" style={styles}>
-          {(!!attachmentFiles && attachmentFiles.length > 0) && renderPreviewFiles()}
+        <FlipMove enterAnimation='fade' leaveAnimation='fade' style={styles}>
+          {!!attachmentFiles &&
+            attachmentFiles.length > 0 &&
+            renderPreviewFiles()}
         </FlipMove>
       </Grid>
     </div>
