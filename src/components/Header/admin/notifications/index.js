5bc67248cccc546ce19b05aecc7a246b80c9b930
Mohammed <m.wahdan@devsquads.com>
2020-10-28 11:24:25 +0200

    fix notifications menu z-index

--- src/components/Header/admin/notifications/index.js
+++ src/components/Header/admin/notifications/index.js
@@ -8,12 +8,14 @@ import Paper from '@material-ui/core/Paper';
 import useNotification from '../../../../hooks/notifications/useNotification';
 import NotificationsIcon from './NotificationsIcon';
 import NotificationList from './NotificationList';
+import { useHeaderStyles } from '../../../../styles/Admin/headerStyles';
 
 const useStyles = makeStyles(styles);
 export const notificationsListId = 'notification-menu-list-grow';
 
 const Notifications = () => {
   const classes = useStyles();
+  const headerClasses = useHeaderStyles();
   const { menuOpened } = useNotification();
   return (
     <div className={classes.manager}>
@@ -24,9 +26,7 @@ const Notifications = () => {
         transition
         disablePortal
         className={
-          classNames({ [classes.popperClose]: !menuOpened }) +
-          ' ' +
-          classes.popperNav
+          classNames({ [classes.popperClose]: !menuOpened, [classes.popperNav]: true, [headerClasses.notificationMenu]: true })
         }>
         {({ TransitionProps, placement }) => {
           return (
---
+++ src/styles/Admin/headerStyles.js
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useHeaderStyles = makeStyles(() => ({
+  notificationMenu: {
+    zIndex: '1',
+  },
+}));
