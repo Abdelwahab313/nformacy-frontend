dccc2e183485cf4e4ae4043d547bfcdce695c85f
mohamedalzaki <mohamedalzaki@outlook.com>
2020-10-29 15:51:02 +0200

    [fields-refactor] Remove subfields if parent field removed.

--- src/components/inputs/FieldsSelect/FieldsSelect.js
+++ src/components/inputs/FieldsSelect/FieldsSelect.js
@@ -13,23 +13,38 @@ const getSubFieldsOptions = (majorField) => {
   });
 };
 const getMajorFieldsFromSubfields = (majorFieldsOptions, subfields) => {
-  let majorFieldsIds = subfields?.map((subfield) => (subfield.majorFieldId));
+  let majorFieldsIds = subfields?.map((subfield) => subfield.majorFieldId);
   majorFieldsIds = [...new Set(majorFieldsIds)];
-  return majorFieldsOptions.filter((majorField) => majorFieldsIds.includes(majorField.id));
+  return majorFieldsOptions.filter((majorField) =>
+    majorFieldsIds.includes(majorField.id),
+  );
 };
-
 
 const FieldsSelect = ({ initialFields, updateFields, children }) => {
   const [selectedMajorFields, setSelectedMajorFields] = useState();
   const { fields: majorFieldsOptions, loading } = useFieldFetcher();
 
   useEffect(() => {
-    const initialMajorFieldValue = getMajorFieldsFromSubfields(majorFieldsOptions, initialFields);
-    setSelectedMajorFields(initialMajorFieldValue);
-  }, [majorFieldsOptions, initialFields]);
+    if (!loading) {
+      const initialMajorFieldValue = getMajorFieldsFromSubfields(
+        majorFieldsOptions,
+        initialFields,
+      );
+      setSelectedMajorFields(initialMajorFieldValue);
+    }
+  }, [majorFieldsOptions, loading]);
 
   const handleMajorFieldChange = (selectedList) => {
+    console.log('---Change ', selectedList);
     setSelectedMajorFields(selectedList);
+    const majorFieldIds = selectedList.map(
+      (majorField) => majorField.id,
+    );
+    const updatedSubFields = initialFields.filter((field) =>
+      majorFieldIds.includes(field.majorFieldId),
+    );
+    console.log('---UPDATED SUB FIELDS', updatedSubFields);
+    updateFields([...updatedSubFields]);
   };
 
   // @selectedList [{id,label,majorFieldId}]
@@ -42,14 +57,18 @@ const FieldsSelect = ({ initialFields, updateFields, children }) => {
   const availableSubFieldsOptions = useMemo(() => {
     // console.log('-------', selectedMajorFields);
     if (!!selectedMajorFields && selectedMajorFields.length > 0) {
-      const selectedMajorFieldsIDs = selectedMajorFields.map((majorField) => majorField.id);
+      const selectedMajorFieldsIDs = selectedMajorFields.map(
+        (majorField) => majorField.id,
+      );
 
       const availableMajorFields = majorFieldsOptions.filter((majorField) => {
         return selectedMajorFieldsIDs.includes(majorField.id);
       });
       // console.log('-------+++', selectedMajorFieldsIDs, majorFieldsOptions, availableMajorFields);
 
-      const availableSubFields = availableMajorFields?.map((majorField) => (getSubFieldsOptions(majorField))).flat();
+      const availableSubFields = availableMajorFields
+        ?.map((majorField) => getSubFieldsOptions(majorField))
+        .flat();
       // console.log('available subfields', availableSubFields);
       return availableSubFields;
     } else {
@@ -59,28 +78,30 @@ const FieldsSelect = ({ initialFields, updateFields, children }) => {
 
   // console.log('--------', availableSubFieldsOptions);
 
+  const MajorField = () => (
+    <AutoCompleteSelectField
+      id='majorFieldsOfExperienceSelect'
+      name='majorFieldsOfExperience'
+      inputLabel={t['majorFieldOfExperience']}
+      options={majorFieldsOptions}
+      value={selectedMajorFields}
+      onChange={handleMajorFieldChange}
+      loading={loading}
+    />
+  );
 
-  const MajorField = () => (<AutoCompleteSelectField
-    id='majorFieldsOfExperienceSelect'
-    name='majorFieldsOfExperience'
-    inputLabel={t['majorFieldOfExperience']}
-    options={majorFieldsOptions}
-    value={selectedMajorFields}
-    onChange={handleMajorFieldChange}
-    loading={loading}
-  />);
-
-
-  const Field = () => (<AutoCompleteSelectField
-    name='specificFieldsOfExperience'
-    id='specificFieldsOfExperienceSelect'
-    inputLabel={t['specificField']}
-    options={availableSubFieldsOptions}
-    value={initialFields}
-    onChange={handleFieldsChange}
-    groupBy={(option) => option.majorFieldLabel}
-    loading={loading}
-  />);
+  const Field = () => (
+    <AutoCompleteSelectField
+      name='specificFieldsOfExperience'
+      id='specificFieldsOfExperienceSelect'
+      inputLabel={t['specificField']}
+      options={availableSubFieldsOptions}
+      value={initialFields}
+      onChange={handleFieldsChange}
+      groupBy={(option) => option.majorFieldLabel}
+      loading={loading}
+    />
+  );
 
   return children({ MajorField, Field });
 };
