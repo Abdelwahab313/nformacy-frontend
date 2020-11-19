1a74b8c13617e8fdb55ccf4ed3ee512fafb90869
amal@devsquads.com <amal@devsquads.com>
2020-11-19 18:14:20 +0200

    WIP-Client home page

--- src/pages/App/Home/index.js
+++ src/pages/App/Home/index.js
@@ -1,6 +1,6 @@
+
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { Box, Grid } from '@material-ui/core';
+import { Grid, Button, InputBase, Divider, Link } from '@material-ui/core';
 import ProfileSummaryCard from './subComponents/ProfileSummaryCard';
 import useStyles from './styles/HomePageStyles';
 import CalendarCard from './subComponents/CalendarCard';
@@ -8,6 +8,7 @@ import { useAuth } from '../../auth/context/auth';
 import authManager from '../../../services/authManager';
 import AvailableServiceSection from './subComponents/AvailableServices';
 import { useTranslation } from 'react-i18next';
+import SubmitButton from 'components/buttons/SubmitButton';
 
 const HomePage = () => {
   const classes = useStyles();
@@ -15,24 +16,116 @@ const HomePage = () => {
   const { t } = useTranslation();
 
   return (
-    <Grid container>
-      <Grid item lg={3} className={classes.sidebar}>
-        <ProfileSummaryCard/>
-        <CalendarCard currentUser={currentUser}/>
+    <Grid container justify="center" className={classes.clientHomeContainer}>
+
+      <Grid item xs={12} md={3}>
+        <ProfileSummaryCard />
+        <CalendarCard currentUser={currentUser} />
       </Grid>
 
-      <Grid item lg>
-        <Box mt={8}>
-          <Typography variant='h4' align='center'>
-            {t('home:welcomeToMedad')}
-          </Typography>
-        </Box>
-        {authManager.isClient() && (<AvailableServiceSection/>)}
+      <Grid item xs={12} md={8}>
+
+        <Grid container>
+
+          <Grid item xs={12} md={8} className={classes.askQuestionBox}>
+            <Grid container className={classes.askExpertContainer}>
+              <Grid xs={2} md={1}>
+                <img
+                  color={'primary'}
+                  src={require('../../../assets/question.svg')}
+                  width={'50%'}
+                />
+              </Grid>
+              <Grid xs={10} md={11}>
+                <form>
+                  <InputBase
+                    className={classes.askExpertInputField}
+                    placeholder="Ask the expert"
+                  />
+                </form>
+              </Grid>
+            </Grid>
+            <Divider className={classes.dividers} />
+
+            <Grid container>
+              <Grid item xs={8} md={8}>
+                <Link
+                  underline="none"
+                  className={classes.askQuestionLink}
+                  href="#" onClick={() => { }}>
+                  {t('writeGreatQuestion')}
+                </Link>
+                <Divider className={[classes.dividers, classes.writeQuestionBorder]} />
+              </Grid>
+
+              <Grid item xs={4} md={4} justify="center" >
+                <SubmitButton
+                  id={'proceedBtn'}
+                  onClick={() => { }}
+                  className={classes.proceedBtn}
+                  buttonText={'proceed'}
+                />
+              </Grid>
+            </Grid>
+
+          </Grid>
+
+          <Grid item xs={12} md={3} className={classes.askQuestionBox}>
+            <div>you have 40 points</div>
+            <Button>proceed</Button>
+          </Grid>
+        </Grid>
+
+        <Grid container>
+          <Grid item xs={12} md={3} className={classes.askQuestionBox}>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <h5>call the expert</h5>
+                <p>test test</p>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <img
+                  color={'primary'}
+                  src={require('../../../assets/project.svg')}
+                  width={'100%'}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={3} className={classes.askQuestionBox}>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <h5>call the expert</h5>
+                <p>test test</p>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <img src=""></img>
+              </Grid>
+              <Button>start the process</Button>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={12} md={3} className={classes.askQuestionBox}>
+            <Grid container>
+              <Grid item xs={12} md={6}>
+                <h5>call the expert</h5>
+                <p>test test</p>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <img src=""></img>
+              </Grid>
+            </Grid>
+          </Grid>
+
+        </Grid>
+
+        {authManager.isClient() && (<AvailableServiceSection />)}
       </Grid>
-      <Grid item lg={3}>
-      </Grid>
+
     </Grid>
   );
 };
+
 
 export default HomePage;
--- src/pages/App/Home/styles/HomePageStyles.js
+++ src/pages/App/Home/styles/HomePageStyles.js
@@ -1,6 +1,5 @@
 import { makeStyles } from '@material-ui/core';
 
-
 const useStyles = makeStyles((theme) => ({
   card: {
     maxWidth: 345,
@@ -11,9 +10,57 @@ const useStyles = makeStyles((theme) => ({
   media: {
     height: 140,
   },
-  sidebar: {
-    padding: theme.spacing(3),
+  clientHomeContainer: {
+    padding: theme.spacing(5),
+    [theme.breakpoints.down('md')]: {
+      padding: theme.spacing(3)
+    },
   },
+  askQuestionBox: {
+    padding: theme.spacing(3),
+    margin: theme.spacing(2),
+    backgroundColor: '#fafafa',
+    borderRadius: 8,
+    border: '1px solid #ecedf0',
+    [theme.breakpoints.down('md')]: {
+      margin: '10px 0'
+    },
+  },
+  underline: {
+    color: 'red'
+  },
+  askExpertInputField: {
+    width: '100%'
+  },
+  proceedBtn: {
+    marginTop: '12px',
+    width: '60%',
+    float: 'right',
+    borderRadius: '11px',
+    [theme.breakpoints.down('md')]: {
+      fontSize: 12,
+      textTransform: 'capitalize'
+    },
+  },
+  askQuestionLink: {
+    marginTop: theme.spacing(2),
+    display: 'block',
+    [theme.breakpoints.down('md')]: {
+      fontSize: 15
+    },
+  },
+  askExpertContainer: {
+    padding: [theme.spacing(4), theme.spacing(2)]
+  },
+  dividers: {
+    margin: [theme.spacing(1), theme.spacing(0)]
+  },
+  writeQuestionBorder: {
+    width: '35%',
+    [theme.breakpoints.down('md')]: {
+      width: '83%'
+    },
+  }
 }));
 
 export default useStyles;
