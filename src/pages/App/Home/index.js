a6980198e277adf98c14d87543dcff8cc578e9b3
amal@devsquads.com <amal@devsquads.com>
2020-11-24 14:54:43 +0200

    handle transilation in client homepage

--- src/pages/App/Home/index.js
+++ src/pages/App/Home/index.js
@@ -10,6 +10,7 @@ import { useTranslation } from 'react-i18next';
 import SubmitButton from 'components/buttons/SubmitButton';
 import ActivityTable from './subComponents/ActivityTable';
 import FeedsTimeline from './subComponents/FeedsTimeline';
+import Direction from 'components/grid/Direction';
 
 const HomePage = () => {
   const classes = useStyles();
@@ -34,100 +35,102 @@ const HomePage = () => {
           {currentUser.firstName + ' ' + currentUser.lastName}
         </Typography>
       </Card>
-      <Grid container className={classes.clientHomeContainer}>
-        <Grid item xs={12} md={3} className={classes.leftSectionContainer}>
-          <ProfileSummaryCard />
-          <CalendarCard currentUser={currentUser} />
-        </Grid>
-        <Grid item xs={12} md={9}>
-          <Grid container justify='space-around'>
-            <Grid item xs={12} md={8} className={classes.askQuestionBox}>
-              <Grid container className={classes.askExpertContainer}>
-                <Grid xs={2} md={1}>
-                  <img
-                    color={'primary'}
-                    src={require('../../../assets/question.svg')}
-                    width={'50%'}
-                  />
-                </Grid>
-                <Grid xs={10} md={11}>
-                  <form>
-                    <InputBase
-                      className={classes.askExpertInputField}
-                      placeholder='Ask the expert'
+      <Direction>
+        <Grid container className={classes.clientHomeContainer}>
+          <Grid item xs={12} md={3} className={classes.leftSectionContainer}>
+            <ProfileSummaryCard />
+            <CalendarCard currentUser={currentUser} />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Grid container justify='space-around'>
+              <Grid item xs={12} md={8} className={classes.askQuestionBox}>
+                <Grid container className={classes.askExpertContainer}>
+                  <Grid xs={2} md={1}>
+                    <img
+                      color={'primary'}
+                      src={require('../../../assets/question.svg')}
+                      width={'50%'}
                     />
-                  </form>
+                  </Grid>
+                  <Grid xs={10} md={11}>
+                    <form>
+                      <InputBase
+                        className={classes.askExpertInputField}
+                        placeholder={t('askTheExpert')}
+                      />
+                    </form>
+                  </Grid>
+                </Grid>
+                <Divider className={classes.dividers} />
+
+                <Grid container>
+                  <Grid item xs={8} md={8}>
+                    <Link
+                      underline='none'
+                      className={classes.askQuestionLink}
+                      href='#'
+                      onClick={() => { }}>
+                      {t('writeGreatQuestion')}
+                    </Link>
+                    <Divider
+                      className={[classes.dividers, classes.writeQuestionBorder]}
+                    />
+                  </Grid>
+
+                  <Grid item xs={4} md={4} justify='center'>
+                    <SubmitButton
+                      id={'proceedBtn'}
+                      onClick={() => { }}
+                      className={classes.proceedBtn}
+                      buttonText={t('proceed')}
+                    />
+                  </Grid>
                 </Grid>
               </Grid>
-              <Divider className={classes.dividers} />
 
-              <Grid container>
-                <Grid item xs={8} md={8}>
-                  <Link
-                    underline='none'
-                    className={classes.askQuestionLink}
-                    href='#'
-                    onClick={() => { }}>
-                    {t('writeGreatQuestion')}
-                  </Link>
-                  <Divider
-                    className={[classes.dividers, classes.writeQuestionBorder]}
-                  />
-                </Grid>
-
-                <Grid item xs={4} md={4} justify='center'>
-                  <SubmitButton
-                    id={'proceedBtn'}
-                    onClick={() => { }}
-                    className={classes.proceedBtn}
-                    buttonText={'proceed'}
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid
-              item
-              xs={12}
-              md={3}
-              className={[classes.askQuestionBox, classes.pointsBox]}>
               <Grid
-                container
-                direction='column'
-                alignItems='center'
-                justify='space-between'>
-                <div className={classes.marginBottom}>
-                  <img
-                    className={[classes.walletIcon]}
-                    color={'primary'}
-                    src={require('../../../assets/wallet.svg')}
-                    width={'25%'}
-                  />
-                </div>
-                <div className={classes.marginBottom}>you have 40 points</div>
-                <div className={classes.marginBottom}>
-                  <SubmitButton
-                    id={'proceedBtn'}
-                    onClick={() => { }}
-                    className={[classes.chargeBtn, classes.marginBottom]}
-                    buttonText={'Charge my wallet'}
-                  />
-                </div>
+                item
+                xs={12}
+                md={3}
+                className={[classes.askQuestionBox, classes.pointsBox]}>
+                <Grid
+                  container
+                  direction='column'
+                  alignItems='center'
+                  justify='space-between'>
+                  <div className={classes.marginBottom}>
+                    <img
+                      className={[classes.walletIcon]}
+                      color={'primary'}
+                      src={require('../../../assets/wallet.svg')}
+                      width={'25%'}
+                    />
+                  </div>
+                  <div className={classes.marginBottom}>you have 40 points</div>
+                  <div className={classes.marginBottom}>
+                    <SubmitButton
+                      id={'proceedBtn'}
+                      onClick={() => { }}
+                      className={[classes.chargeBtn, classes.marginBottom]}
+                      buttonText={t('chargeMyWallet')}
+                    />
+                  </div>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
 
-          <Grid className={classes.threeBtnsContainer}>
-            {authManager.isClient() && <AvailableServiceSection />}
-          </Grid>
-          <Grid item xs={12} md={12} className={classes.sectionContainer}>
-            <ActivityTable />
-          </Grid>
-          <Grid item xs={12} md={12} className={classes.sectionContainer}>
-            <FeedsTimeline />
+            <Grid className={classes.threeBtnsContainer}>
+              {authManager.isClient() && <AvailableServiceSection />}
+            </Grid>
+            <Grid item xs={12} md={12} className={classes.sectionContainer}>
+              <ActivityTable />
+            </Grid>
+            <Grid item xs={12} md={12} className={classes.sectionContainer}>
+              <FeedsTimeline />
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Direction>
     </Grid>
   );
 };
