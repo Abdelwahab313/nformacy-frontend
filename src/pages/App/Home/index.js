9ff856bb04d853cca984ca4c37dd648d0252598e
Abdelwahab <Abdelwahab@devsquads.com>
2020-11-25 12:27:43 +0200

    add consultant home page

--- src/pages/App/Home/index.js
+++ src/pages/App/Home/index.js
@@ -1,85 +1,14 @@
 import React from 'react';
-import { Grid, Box } from '@material-ui/core';
-import ProfileSummaryCard from './subComponents/ProfileSummaryCard';
-import useStyles from './styles/HomePageStyles';
-import CalendarCard from './subComponents/CalendarCard';
-import { useAuth } from '../../auth/context/auth';
-import AvailableServiceSection from './subComponents/AvailableServices';
-import { useTranslation } from 'react-i18next';
-import SubmitButton from 'components/buttons/SubmitButton';
-import ActivityTable from './subComponents/ActivityTable';
-import FeedsTimeline from './subComponents/FeedsTimeline';
-import Direction from 'components/grid/Direction';
-import LibraryCard from './subComponents/LibraryCard';
-import { CalendarLibraryForMobile } from './subComponents/CalendarLibraryForMobile';
-import HomeHeadBar from './subComponents/HomeHeadBar';
-import AskTheExpertSection from './subComponents/AskTheExpertSection';
+import authManager from 'services/authManager';
+import ClientHomePage from './ClientHomePage';
+import ConsultantHomePage from './ConsultantHomePage';
 
 const HomePage = () => {
-  const classes = useStyles();
-  const [{ currentUser }] = useAuth();
-  const { t } = useTranslation();
-
-  return (
-    <Direction>
-      <Grid container justify='center'>
-        <HomeHeadBar />
-        <Grid container className={classes.clientHomeContainer} spacing={4}>
-          <Grid item xs={12} md={3}>
-            <ProfileSummaryCard />
-            <CalendarCard currentUser={currentUser} />
-            <LibraryCard />
-          </Grid>
-          <Grid item xs={12} md={9}>
-            <Grid container spacing={4}>
-              <Grid item xs={12} md={9}>
-                <AskTheExpertSection />
-              </Grid>
-
-              <Grid item xs={12} md={3}>
-                <Box className={[classes.askQuestionBox, classes.pointsBox]}>
-                  <Grid
-                    container
-                    direction='column'
-                    alignItems='center'
-                    justify='space-evenly'
-                    className={classes.pointsContainer}>
-                    <img
-                      className={[classes.walletIcon]}
-                      color={'primary'}
-                      src={require('../../../assets/wallet.svg')}
-                      width={'25%'}
-                    />
-                    <div className={classes.marginBottom}>
-                      you have 40 points
-                    </div>
-                    <SubmitButton
-                      id={'proceedBtn'}
-                      onClick={() => {}}
-                      className={[classes.chargeBtn]}
-                      buttonText={t('chargeMyWallet')}
-                    />
-                  </Grid>
-                </Box>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} md={12} className={classes.sectionContainer}>
-              <AvailableServiceSection />
-            </Grid>
-            <Grid item xs={12} md={12} className={classes.sectionContainer}>
-              <ActivityTable />
-            </Grid>
-            <Grid item xs={12} md={12} className={classes.sectionContainer}>
-              <CalendarLibraryForMobile />
-            </Grid>
-            <Grid item xs={12} md={12} className={classes.sectionContainer}>
-              <FeedsTimeline />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Direction>
-  );
+  if (authManager.isClient()) {
+    return <ClientHomePage />;
+  } else {
+    return <ConsultantHomePage />;
+  }
 };
 
 export default HomePage;
