da8c8e62ab7eb5dcafb00a51362d827664792e1c
Abdelwahab <Abdelwahab@devsquads.com>
2020-12-17 20:58:51 +0200

    update translations from the exprted excel

--- parseTranslations.js
+++ parseTranslations.js
@@ -2,25 +2,59 @@ let en_json = require('./public/locales/en/common.json');
 let ar_json = require('./public/locales/ar/common.json');
 
 const mergeTranslation = () => {
-  // console.log('en_json count ', Object.keys(en_json).length);
-  // console.log('ar_json count ', Object.keys(ar_json).length);
-  let output = {};
-  // for (key in en_json) {
-  //   output[key] = [en_json[key], ar_json[key] || ''];
-  // }
+  const mergedTranslationJson = Object.keys(en_json).map((key) => ({
+    key,
+    english: en_json[key],
+    arabic: ar_json[key] || '',
+  }));
 
-  output = Object.keys(en_json).map((key) => ([key, en_json[key], ar_json[key] || '']));
+  console.log('output count ', Object.keys(output).length);
 
-  // console.log('output --------', output);
-  // console.log('output count ', Object.keys(output).length);
+  const csv = require('fast-csv');
+
+  let rowCount = 1;
+  const csvStream = csv.format({ headers: true });
+  csvStream.pipe(process.stdout).on('end', () => process.exit());
+  mergedTranslationJson.map((entry) => {
+    rowCount += 1;
+    csvStream.write(entry);
+  });
+  csvStream.end();
+
+  console.log(`Parsed ${rowCount} entry`);
 
   const FileSystem = require('fs');
-  FileSystem.writeFile('translations.csv', JSON.stringify(output), (e) => {
+  FileSystem.writeFile('translations.csv', csvStream, (e) => {
     if (e) throw e;
   });
 };
 
-mergeTranslation();
+const parseTranslationToJson = () => {
+  const fs = require('fs');
+  const path = require('path');
+  const csv = require('fast-csv');
+  const parsedData = [];
+  fs.createReadStream(path.resolve(__dirname, 'translated.csv'))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', (error) => console.error(error))
+    .on('data', (row) => {
+      const { key, english, arabic } = row;
+      en_json[key] = english;
+      ar_json[key] = arabic;
+      console.log(row);
+      parsedData.push(row);
+    })
+    .on('end', (rowCount) => {
+      console.log(`Parsed ${rowCount} rows`);
+      fs.writeFile('output_en.json', JSON.stringify(en_json), (e) => {
+        if (e) throw e;
+      });
+      fs.writeFile('output_ar.json', JSON.stringify(ar_json), (e) => {
+        if (e) throw e;
+      });
+    });
+  console.log('read successfully');
+};
 
-// const loadTranslationFromMergedFiles = () => {
-// };
+// parseTranslationToJson();
+// mergeTranslation();
