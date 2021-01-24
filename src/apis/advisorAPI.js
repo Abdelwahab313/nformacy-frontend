e330b43433d687b2957171436f4c97f20a7daebb
amal@devsquads.com <amal@devsquads.com>
2021-01-24 19:55:13 +0200

    fix data for advisors

--- src/apis/advisorAPI.js
+++ src/apis/advisorAPI.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../settings';
-import { camelizeKeys } from 'humps';
+import { camelizeKeys, decamelizeKeys } from 'humps';
 
 export const addAdvisor = (user) => {
   return axios({
     method: 'post',
-    data: user,
+    data: decamelizeKeys({ ...user }),
     url: `${API_BASE_URL}/users/create_advisor`,
   }).then((response) => camelizeKeys(response));
 };
