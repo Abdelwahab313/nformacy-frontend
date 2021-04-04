3427780220207242b24239a524d1024633c28450
Abdelwahab <Abdelwahab@devsquads.com>
2021-04-04 20:08:51 +0200

    remove learn more button to points box

--- src/pages/App/Home/subComponents/ConsultantPointsBox.js
+++ src/pages/App/Home/subComponents/ConsultantPointsBox.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Grid, Box } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 
-import SubmitButton from 'components/buttons/SubmitButton';
 import useStyles from '../styles/HomePageStyles';
 import CustomTypography from 'components/typography/Typography';
 import authManager from 'services/authManager';
@@ -34,16 +33,6 @@ const ConsultantPointsBox = () => {
         <CustomTypography variant='body1' fontWeight='bold'>
           {`${t('youHave')} ${earnedPoints} ${t('points')}`}
         </CustomTypography>
-        <SubmitButton
-          id={'proceedBtn'}
-          onClick={() => {}}
-          className={[classes.chargeBtn]}
-          buttonText={
-            <CustomTypography variant='body1'>
-              {t('learnMore')}
-            </CustomTypography>
-          }
-        />
       </Grid>
     </Box>
   );
