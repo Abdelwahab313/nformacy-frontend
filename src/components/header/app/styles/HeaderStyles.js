c1c6ad2d34d0348b011954fae95bd118f2720808
amal@devsquads.com <amal@devsquads.com>
2021-05-24 10:54:15 +0200

    display menue in 1024 screen

--- src/components/header/app/styles/HeaderStyles.js
+++ src/components/header/app/styles/HeaderStyles.js
@@ -66,7 +66,7 @@ const useStyles = makeStyles((theme) => ({
   },
   sectionMobile: {
     display: 'flex',
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('lg')]: {
       display: 'none',
     },
   },
