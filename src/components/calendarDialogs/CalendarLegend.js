04b7d0e29b5ed4c93a805639dd3e3b5b5e93ed5e
amal@devsquads.com <amal@devsquads.com>
2021-05-26 15:10:52 +0200

    added call and meeting times in legand

--- src/components/calendarDialogs/CalendarLegend.js
+++ src/components/calendarDialogs/CalendarLegend.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, Box, Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
-import { darkBlue } from 'styles/colors';
+import { darkBlue, turquoise, lightOrange } from 'styles/colors';
 
 const CalendarLegend = () => {
   const classes = useStyles();
@@ -11,20 +11,45 @@ const CalendarLegend = () => {
   return (
     <div className={classes.calendarLegend}>
       <Grid container>
-        <Grid item xs={2} align="center">
-          <Box className={classes.calendarLegendColorBox}></Box>
+        <Grid item xs={4}>
+          <Grid container className={classes.freeTime}>
+            <Grid item xs={2} align="center">
+              <Box className={[classes.calendarLegendColorBox, classes.freeTimeColorBox]}></Box>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant={'body2'}>{t('freeTime')}</Typography>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={10}>
-          <Typography variant={'body2'}>{t('availableTime')}</Typography>
+        <Grid item xs={4}>
+          <Grid container className={classes.freeTime}>
+            <Grid item xs={2} align="center">
+              <Box className={[classes.calendarLegendColorBox, classes.meetingTimeColorBox]}></Box>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant={'body2'}>{t('meetingTime')}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <Grid container className={classes.freeTime}>
+            <Grid item xs={2} align="center">
+              <Box className={[classes.calendarLegendColorBox, classes.callTimeColorBox]}></Box>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography variant={'body2'}>{t('callTime')}</Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
+
     </div>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
   calendarLegend: {
-    width: 300,
+    // width: '100%',
     padding: 10,
     border: '1px solid #e0e0e0',
     marginTop: 30,
@@ -35,9 +60,17 @@ const useStyles = makeStyles((theme) => ({
   calendarLegendColorBox: {
     width: 10,
     height: 10,
-    backgroundColor: darkBlue,
     display: 'inline-block',
     verticalAlign: 'middle',
+  },
+  freeTimeColorBox: {
+    backgroundColor: darkBlue,
+  },
+  meetingTimeColorBox: {
+    backgroundColor: lightOrange,
+  },
+  callTimeColorBox: {
+    backgroundColor: turquoise,
   }
 }));
 export default CalendarLegend;
