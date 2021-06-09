2de0a1b59b89f1559d6c3474408e84d916580a28
amal@devsquads.com <amal@devsquads.com>
2021-06-09 16:28:01 +0200

    WIP-consultant verification

--- src/pages/Admin/Consultants/verification/consultantVerificationForm.js
+++ src/pages/Admin/Consultants/verification/consultantVerificationForm.js
@@ -4,20 +4,33 @@ import GridItem from 'components/grid/GridItem';
 import useFieldFetcher from 'hooks/useFieldsFetcher';
 import LoadingCircle from 'components/progress/LoadingCircle';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, FormControlLabel, Radio, RadioGroup, TextField, InputLabel } from '@material-ui/core';
+import {
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+  InputLabel,
+  TextareaAutosize,
+} from '@material-ui/core';
 import authManager from 'services/authManager';
 import { getMajorFieldsFromSubfields } from 'core/fields';
+import SubmitButton from 'components/buttons/SubmitButton';
+import { useTranslation } from 'react-i18next';
 
 const ConsultantVerificationForm = () => {
   const classes = useStyles();
+  const { t } = useTranslation();
   const currentUser = authManager.retrieveCurrentUser();
   const subFields = currentUser.fields;
   const { fields: fieldsLabels, loading } = useFieldFetcher();
   const majorFields = getMajorFieldsFromSubfields(fieldsLabels, subFields);
 
-
-
-  const [consultantEvaluation, setConsultantEvaluation] = useState({ fieldsEvaluation: [], hourlyRate: 0, comment: '', isVerified: false });
+  const [consultantEvaluation, setConsultantEvaluation] = useState({
+    fieldsEvaluation: [],
+    hourlyRate: 0,
+    comment: '',
+    isVerified: false,
+  });
 
   useEffect(() => {
     if (!!majorFields) {
@@ -26,13 +39,31 @@ const ConsultantVerificationForm = () => {
         label: field.label,
         rating: field.rating,
       }));
-      setConsultantEvaluation({ ...consultantEvaluation, fieldsEvaluation: majorFieldsWithRating });
+      setConsultantEvaluation({
+        ...consultantEvaluation,
+        fieldsEvaluation: majorFieldsWithRating,
+      });
     }
   }, [majorFields?.length]);
 
   const handleFieldsEvaluation = (e, fieldId) => {
-    const newFieldsEvaluation = consultantEvaluation?.fieldsEvaluation?.map(field => field.id == fieldId ? { ...field, rating: e.target.value } : field);
-    setConsultantEvaluation({ ...consultantEvaluation, fieldsEvaluation: [...newFieldsEvaluation] });
+    const newFieldsEvaluation = consultantEvaluation?.fieldsEvaluation?.map(
+      (field) =>
+        field.id == fieldId ? { ...field, rating: e.target.value } : field,
+    );
+    setConsultantEvaluation({
+      ...consultantEvaluation,
+      fieldsEvaluation: [...newFieldsEvaluation],
+    });
+  };
+
+  const onSubmit = () => {
+    setConsultantEvaluation({
+      fieldsEvaluation: [],
+      hourlyRate: 0,
+      comment: '',
+      isVerified: false,
+    });
   };
 
   if (loading)
@@ -41,100 +72,154 @@ const ConsultantVerificationForm = () => {
     );
 
   return (
-    <form noValidate autoComplete="off">
+    <form noValidate autoComplete='off'>
       {consultantEvaluation?.fieldsEvaluation?.map((field, key) => {
         return (
           <Fragment>
-            <Grid container>
-              <Grid item xs={6}>
+            <GridContainer>
+              <GridItem xs={4}>
                 <div key={key}>{field.label}</div>
-              </Grid>
-              <Grid item xs={6}>
-                <RadioGroup row aria-label="position" name="position" onChange={(e) => handleFieldsEvaluation(e, field.id)}>
+              </GridItem>
+              <GridItem xs={8}>
+                <RadioGroup
+                  row
+                  aria-label='position'
+                  name='position'
+                  onChange={(e) => handleFieldsEvaluation(e, field.id)}>
                   <GridContainer>
                     <GridItem xs={3}>
                       <FormControlLabel
-                        value="0"
-                        control={<Radio color="primary" />}
-                        label="Junior"
-                        labelPlacement="end"
+                        value='0'
+                        control={<Radio color='primary' />}
+                        label='Junior'
+                        labelPlacement='end'
                       />
                     </GridItem>
                     <GridItem xs={3}>
                       <FormControlLabel
-                        value="1"
-                        control={<Radio color="primary" />}
-                        label="Mid"
-                        t="end"
+                        value='1'
+                        control={<Radio color='primary' />}
+                        label='Mid'
+                        t='end'
                       />
                     </GridItem>
                     <GridItem xs={3}>
                       <FormControlLabel
-                        value="2"
-                        control={<Radio color="primary" />}
-                        label="Expert"
-                        labelPlacement="end"
+                        value='2'
+                        control={<Radio color='primary' />}
+                        label='Expert'
+                        labelPlacement='end'
                       />
                     </GridItem>
                     <GridItem xs={3}>
                       <FormControlLabel
-                        value="3"
-                        control={<Radio color="primary" />}
-                        label="Guru"
-                        labelPlacement="end"
+                        value='3'
+                        control={<Radio color='primary' />}
+                        label='Guru'
+                        labelPlacement='end'
                       />
                     </GridItem>
                   </GridContainer>
                 </RadioGroup>
-              </Grid>
-            </Grid>
+              </GridItem>
+            </GridContainer>
           </Fragment>
         );
       })}
 
-      <GridContainer>
-        <GridItem xs={3}>
-          <InputLabel shrink>Hourly Rated Requested:</InputLabel>
+      <GridContainer className={classes.consultantEvaluationSections}>
+        <GridItem xs={4}>
+          <InputLabel className={classes.formLabels} shrink>
+            {t('hourlyRatedRequested')}
+          </InputLabel>
         </GridItem>
-        <GridItem xs={9}>
-          <TextField id="outlined-basic" variant="outlined" />
+        <GridItem xs={8}>
+          <TextField
+            name='hourlyRate'
+            type='number'
+            onChange={(e) =>
+              setConsultantEvaluation({
+                ...consultantEvaluation,
+                hourlyRate: e.target.value,
+              })
+            }
+            value={consultantEvaluation.hourlyRate}
+            id='outlined-basic'
+            variant='outlined'
+          />
         </GridItem>
       </GridContainer>
-      <br />
-      <GridContainer>
-        <GridItem xs={3}>
-          <InputLabel shrink>Comments:</InputLabel>
+
+      <GridContainer className={classes.consultantEvaluationSections}>
+        <GridItem xs={4}>
+          <InputLabel className={classes.formLabels} shrink>
+            {t('comment')}
+          </InputLabel>
         </GridItem>
-        <GridItem xs={9}>
-          <TextField id="outlined-basic" variant="outlined" />
+        <GridItem xs={8}>
+          <TextareaAutosize
+            rowsMin={2}
+            rowsMax={4}
+            name='comment'
+            onChange={(e) =>
+              setConsultantEvaluation({
+                ...consultantEvaluation,
+                comment: e.target.value,
+              })
+            }
+            value={consultantEvaluation.comment}
+          />
         </GridItem>
       </GridContainer>
-      <br />
-      <GridContainer>
-        <GridItem xs={3}>
-          <InputLabel shrink>Confirm as a Verified Consultant:</InputLabel>
+
+      <GridContainer className={classes.consultantEvaluationSections}>
+        <GridItem xs={4}>
+          <InputLabel className={classes.formLabels} shrink>
+            {t('confirmVerifiedConsultant')}
+          </InputLabel>
         </GridItem>
-        <GridItem xs={9}>
-          <RadioGroup row aria-label="position" name="position">
+        <GridItem xs={8}>
+          <RadioGroup
+            row
+            aria-label='position'
+            onChange={(e) =>
+              setConsultantEvaluation({
+                ...consultantEvaluation,
+                isVerified: e.target.value,
+              })
+            }
+            name='position'>
             <GridContainer>
               <GridItem xs={6}>
                 <FormControlLabel
-                  value="yes"
-                  control={<Radio color="primary" />}
-                  label="Yes"
-                  labelPlacement="end"
+                  value='yes'
+                  control={<Radio color='primary' />}
+                  label='Yes'
+                  labelPlacement='end'
                 />
               </GridItem>
             </GridContainer>
             <GridItem xs={6}>
               <FormControlLabel
-                value="no"
-                control={<Radio color="primary" />}
-                label="No"
-                labelPlacement="end"
+                value='no'
+                control={<Radio color='primary' />}
+                label='No'
+                labelPlacement='end'
               />
             </GridItem>
           </RadioGroup>
+        </GridItem>
+      </GridContainer>
+
+      <GridContainer className={classes.consultantEvaluationSections}>
+        <GridItem xs={12}>
+          <SubmitButton
+            id={'confirm'}
+            onClick={onSubmit}
+            size='large'
+            className={classes.confirm}
+            buttonText={t('confirm')}
+          />
         </GridItem>
       </GridContainer>
     </form>
@@ -150,6 +235,15 @@ const useStyles = makeStyles(() => ({
     alignItems: 'center',
     alignContent: 'center',
   },
+  consultantEvaluationSections: {
+    marginTop: 30,
+  },
+  confirm: {
+    float: 'right',
+  },
+  formLabels: {
+    fontSize: 20,
+  },
 }));
 
-export default ConsultantVerificationForm; 
+export default ConsultantVerificationForm;
