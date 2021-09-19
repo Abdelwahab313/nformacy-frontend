f6ea8e88cea21b0ee95d3885db783ef468392950
amlmoustafa <amal@devsquads.com>
2021-09-19 15:24:45 +0200

    handle scroll notifications menu

--- src/styles/Admin/headerStyles.js
+++ src/styles/Admin/headerStyles.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useHeaderStyles = makeStyles((theme) => ({
   notificationMenu: {
     zIndex: '1',
+    height: '100vh',
+    overflowY: 'auto',
     [theme.breakpoints.down('md')]: {
       maxWidth: 275,
     },
