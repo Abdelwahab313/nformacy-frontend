b3997b7bb6968a797c6b88ebcf32e03315c2e4a8
Abdelwahab <abdelwahabahmed93@gmail.com>
2021-09-21 18:57:28 +0200

    fix error message display for inline fields

--- src/components/errors/ErrorMessage.js
+++ src/components/errors/ErrorMessage.js
@@ -1,9 +1,13 @@
+import { makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
 import React from 'react';
 
 const ErrorMessage = ({ errorField, className }) => {
+  const classes = useStyles();
+
   if (!!errorField) {
     return (
-      <span style={{ color: 'red' }} className={className}>
+      <span className={clsx(classes.errorSpan, className)}>
         {errorField.message}
       </span>
     );
@@ -11,5 +15,12 @@ const ErrorMessage = ({ errorField, className }) => {
     return null;
   }
 };
+
+const useStyles = makeStyles(() => ({
+  errorSpan: {
+    color: 'red',
+    display: 'inline-block',
+  },
+}));
 
 export default ErrorMessage;
