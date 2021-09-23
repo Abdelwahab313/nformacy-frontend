9b57f45537c3adf8fd4d4c485427a6a20f7fa466
amlmoustafa <amal@devsquads.com>
2021-09-23 12:18:58 +0200

    remove unused code

--- src/pages/Admin/Clients/edit/subComponents.js/BeneficiaryProfileDetailsForm.js
+++ src/pages/Admin/Clients/edit/subComponents.js/BeneficiaryProfileDetailsForm.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import { FormContext, useForm } from 'react-hook-form';
 import { saveButtonStyle, useStyles } from 'styles/formsStyles';
-import { updateProfile, updateProfilePicture } from 'apis/userAPI';
+import { updateProfile } from 'apis/userAPI';
 import BeneficiaryBasicInfo from './BeneficiaryBasicInfo';
 
-const BeneficiaryProfileDetailsForm = ({
-  client,
-  closeDialog,
-  setProfilePic,
-  setClient,
-}) => {
+const BeneficiaryProfileDetailsForm = ({ client, closeDialog, setClient }) => {
   const formMethod = useForm({
     defaultValues: { ...client },
   });
-  const [avatar, setAvatar] = useState([]);
 
   const classes = useStyles();
 
@@ -26,24 +20,12 @@ const BeneficiaryProfileDetailsForm = ({
     updateProfile(userToBeSubmitted, client.id).then((response) => {
       setClient(response.data);
     });
-    if (avatar.length > 0) {
-      const file = new Blob(avatar);
-      const formData = new FormData();
-      formData.append('avatar', file, avatar[0].name);
-
-      updateProfilePicture(formData, client.id).then((response) => {
-        setClient(response.data);
-        if (response.data.avatar) {
-          setProfilePic(response.data.avatar);
-        }
-      });
-    }
     client = { ...client, ...userData };
     closeDialog();
   };
 
   return (
-    <FormContext client={client} setAvatar={setAvatar} {...formMethod}>
+    <FormContext client={client} {...formMethod}>
       <form
         id='editBasicInfoForm'
         className={classes.nestedForm}
