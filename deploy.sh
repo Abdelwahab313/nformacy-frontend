b82675a97f382be4c658e27e502f7d7bc2775345
Abdelwahab <abdelwahabahmed93@gmail.com>
2022-01-16 22:03:26 +0200

    add deployment script for the new machine
    (cherry picked from commit b795d906f0897f416aa82c2e9de7494b9d0d4ee7)

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
+serverIp="35.87.89.7"
+sshKey="~/.ssh_keys/nformacy-staging-ui.pem"
+
+
 if [[ $1 == "saas" ]]; then
-    serverIp="18.237.74.26"
-    sshKey="~/.ssh_keys/saas-beta.pem"
-    makeCommand="make deploy-whitelabel"
     buildScript="npm run build:staging-saas"
+    site="whitelabel.nformacy"
 else
-    serverIp="52.40.51.201"
-    sshKey="~/.ssh_keys/frontend_nformacy.pem"
-    makeCommand="make deploy-beta"
     buildScript="npm run build:staging-beta"
+    site="beta.nformacy"
 fi
 
 rm -rf build.zip build/   
 $buildScript    
 zip -vr build.zip build/
 
-ssh -i $sshKey  ec2-user@$serverIp 'rm -rf frontend/build frontend/build.zip'
+ssh -i $sshKey  ubuntu@$serverIp "sudo rm -rf /var/www/$site/build.zip"
 
-scp -i $sshKey build.zip ec2-user@$serverIp:frontend
+scp -i $sshKey build.zip ubuntu@$serverIp:/var/www/$site
 
-ssh -i $sshKey ec2-user@$serverIp 'docker stop nformacyUI'       
-ssh -i $sshKey ec2-user@$serverIp "cd frontend && unzip build.zip &&  $makeCommand"
+ssh -i $sshKey ubuntu@$serverIp "cd /var/www/$site && unzip build.zip && mv html html-old && mv build html && rm -rf html-old"
 
